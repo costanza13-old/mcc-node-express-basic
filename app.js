b466475da07b8237525a80a6bc29a2ee3e387155
costanza13 <costanza@gmail.com>
2016-04-27 14:43:57 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+// create a route for the home path
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'My Page'
+    });
+});
+
+app.listen(3000);
+console.log('Server listening at 127.0.0.1:3000');
